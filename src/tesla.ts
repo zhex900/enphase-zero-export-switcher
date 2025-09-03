9b3d1633608ac7862816be3615b30b36667c8fe5
Jake He <jake.he@nextgen.group>
2025-09-04 09:25:18 +1000

    tesla

--- src/tesla.ts
+++ src/tesla.ts
@@ -32,47 +32,50 @@ export const tesla = async ({
   const {
     response: { percentage_charged, grid_power, solar_power, load_power },
   } = await getBatteryLiveStatus({ accessToken: token.access_token, siteId });
-
-  // ONLY in gridProfile === ZERO_EXPORT
-  // AND it is importing from the grid
-  // on a cloudy day, when solar is not generating much,
-  // we want to set the backup reserve percent to normal to prevent grid charging.
-  // if backup_reserve_percent === BACKUP_RESERVE_PERCENT_ZERO_EXPORT, and import grid is a lot
-  // solar is generating less than house consumption, grid import is more than 1kW,
-  // we want to set the backup reserve percent to normal to prevent grid charging.
   let desiredReserve;
-  if (
-    gridProfile === ZERO_EXPORT &&
-    grid_power > 100 && // importing from grid
-    solar_power < load_power && // solar is generating less than house consumption
-    backup_reserve_percent === BACKUP_RESERVE_PERCENT_ZERO_EXPORT // current backup reserve is at max
-  ) {
-    console.log(
-      `Importing from grid and solar is generating less than house consumption; setting to ${BACKUP_RESERVE_PERCENT_NORMAL_EXPORT}%`,
-    );
-    desiredReserve = BACKUP_RESERVE_PERCENT_NORMAL_EXPORT;
-  } else {
-    desiredReserve =
-      gridProfile === ZERO_EXPORT
-        ? importPrice <= MINIMUM_IMPORT_PRICE
-          ? BACKUP_RESERVE_PERCENT_ZERO_EXPORT
-          : BACKUP_RESERVE_PERCENT_NORMAL_EXPORT
-        : BACKUP_RESERVE_PERCENT_NORMAL_EXPORT;
-  }
+  if (gridProfile === ZERO_EXPORT) {
+    if (
+      percentage_charged === 100 &&
+      backup_reserve_percent === BACKUP_RESERVE_PERCENT_ZERO_EXPORT
+    ) {
+      desiredReserve = BACKUP_RESERVE_PERCENT_NORMAL_EXPORT;
+    } else {
+      // ONLY in gridProfile === ZERO_EXPORT
+      // AND it is importing from the grid
+      // on a cloudy day, when solar is not generating much,
+      // we want to set the backup reserve percent to normal to prevent grid charging.
+      // if backup_reserve_percent === BACKUP_RESERVE_PERCENT_ZERO_EXPORT, and import grid is a lot
+      // solar is generating less than house consumption, grid import is more than 1kW,
+      // we want to set the backup reserve percent to normal to prevent grid charging.
 
-  // do not adjust backup to higher if current reserve is lower than desired reserve and
-  // battery level is below desiredReserve
-  // this means user manually set the backup reserve percent to a lower value
-  // only change to higher if the charge level is above desiredReserve
-  if (
-    gridProfile === NORMAL_EXPORT &&
-    backup_reserve_percent < desiredReserve &&
-    percentage_charged < desiredReserve
-  ) {
-    console.log(
-      `Current backup reserve ${backup_reserve_percent}% below desired ${desiredReserve}%; battery level is ${Math.round(percentage_charged)}%; skipping`,
-    );
-    return;
+      if (
+        grid_power > 200 && // importing from grid
+        solar_power < load_power && // solar is generating less than house consumption
+        backup_reserve_percent === BACKUP_RESERVE_PERCENT_ZERO_EXPORT // current backup reserve is at max
+      ) {
+        console.log(
+          `Importing from grid and solar is generating less than house consumption; setting to ${BACKUP_RESERVE_PERCENT_NORMAL_EXPORT}%`,
+        );
+        desiredReserve = BACKUP_RESERVE_PERCENT_NORMAL_EXPORT;
+      } else {
+        desiredReserve =
+          importPrice <= MINIMUM_IMPORT_PRICE
+            ? BACKUP_RESERVE_PERCENT_ZERO_EXPORT
+            : BACKUP_RESERVE_PERCENT_NORMAL_EXPORT;
+      }
+    }
+    // do not adjust backup to higher if current reserve is lower than desired reserve and
+    // battery level is below desiredReserve
+    // this means user manually set the backup reserve percent to a lower value
+    // only change to higher if the charge level is above desiredReserve
+    if (backup_reserve_percent < desiredReserve && percentage_charged < desiredReserve) {
+      console.log(
+        `Current backup reserve ${backup_reserve_percent}% below desired ${desiredReserve}%; battery level is ${Math.round(percentage_charged)}%; skipping`,
+      );
+      return;
+    }
+  } else {
+    desiredReserve = BACKUP_RESERVE_PERCENT_NORMAL_EXPORT;
   }
 
   if (backup_reserve_percent !== desiredReserve) {
